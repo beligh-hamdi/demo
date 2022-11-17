f387f5d7bc9ebcde80407e30d974372d5e90611b
Beligh HAMDI <beligh.hamdi@gmail.com>
2022-11-17 12:25:42 -0500

    add h2 filesystem

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,4 @@ INSERT INTO countries (id, name) VALUES (2, 'France');
 INSERT INTO countries (id, name) VALUES (3, 'Brazil');
 INSERT INTO countries (id, name) VALUES (4, 'Italy');
 INSERT INTO countries (id, name) VALUES (5, 'Canada');
+INSERT INTO countries (id, name) VALUES (6, 'Tunisia');
